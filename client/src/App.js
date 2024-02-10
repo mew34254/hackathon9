770736ff7de9ddece11a29d2a31297ee8152e0c6
Matthew Weiss <mew34254@uga.edu>
2024-02-10 01:29:19 -0500

    added more routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 
 import Homepage from './components/Homepage';
+import AboutUs from './components/AboutUs';
+import Contact from './components/Contact';
+import Resources from './components/Resources';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Homepage />} />
+        <Route exact path='/' element={<Homepage />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/resources' element={<Resources />} />
       </Routes>
     </Router>
   );
