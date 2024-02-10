014b5f2c502e39940ceba97fd2362fab6b37353b
Hannah Kelly <hannahkelly21@gmail.com>
2024-02-10 11:39:27 -0500

    copied

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import './AboutUs.css';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+function AboutUs(props) {
+
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default AboutUs;
