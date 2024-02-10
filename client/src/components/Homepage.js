4f07febbda530746dc2b887345554e9319b1cd20
Matthew Weiss <mew34254@uga.edu>
2024-02-09 22:54:25 -0500

    finished setting up router

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,11 +1,13 @@
 import './Homepage.css'
 import { useNavigate } from 'react-router-dom';
-import { useSate, useEffect, createElement } from 'react'; 
+import { useSate, useEffect, createElement } from 'react';
 
-function Homepage(props) { 
+function Homepage(props) {
     return (
         <>
-        
+            <div className="Homepage-container">
+
+            </div>
         </>
     )
 }
