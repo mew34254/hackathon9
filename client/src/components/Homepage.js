273aff43486f8b42779c7e6a4d08bcf53debcd6a
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-02-10 12:27:09 -0500

    moved images folder

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,14 +2,13 @@ import './Homepage.css';
 import { useNavigate } from 'react-router-dom';
 import { useSate, useEffect } from 'react';
 
+
 function Homepage(props) {
     return (
         <>
         <section class="section1"> 
             <div className="Homepage-banner">
-                <img src="images/banner.jpg" alt="banner">
-                </img>
-
+                <img src= "images/banner.jpg" alt="banner"/>
             </div>
        
          </section>
