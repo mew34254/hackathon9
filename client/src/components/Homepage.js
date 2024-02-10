beec3420de13041be51e9b8c3f1a4c78e5bf969d
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-02-09 22:35:52 -0500

    Started homepage js and cs files

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import './Homepage.css'
+import { useNavigate } from 'react-router-dom';
+import { useSate, useEffect, createElement } from 'react'; 
+
+function Homepage(props) { 
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Homepage; 
