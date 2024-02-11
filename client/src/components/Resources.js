b457d22b127deff77df88e1b1e839dc44e13e748
Matthew Weiss <mew34254@uga.edu>
2024-02-11 02:56:38 -0500

    added text to resources

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -8,99 +8,102 @@ function Resources(props) {
 
     return (
         <>
-        <NavBar/>
+            <NavBar />
             <div className="resourcesBanner">
-                <img src= "images/columbiaBlue.png" alt="resourcesBanner"/>
+                <img src="images/columbiaBlue.png" alt="resourcesBanner" />
                 <div className="textContainerResources">
-                        <h1> Resources </h1>
-                        <p> Empowering Survivors: Tools, Tips, and 
-                            Networks to Safeguard Against Financial Scams</p>
+                    <h1> Resources </h1>
+                    <p> Empowering Survivors: Tools, Tips, and
+                        Networks to Safeguard Against Financial Scams</p>
                 </div>
             </div>
 
-     <div class="container">
-        <div className="cardHeader"> 
-        <h2>Organizations</h2>
-        </div>
-    <div class="resourcesGrid">
-        <div class="card">
-            <img src="images/resourcesIcon1.png" alt="Image 1"/>
-            <div className="cardText">
-                <h3><a href="https://www.ftc.gov/" target="_blank">Federal Trade Commission</a></h3>
-                <p>The FTC offers information on how to recognize, avoid, and report scams 
-                    and fraud. Their website provides tips on protecting yourself from identity 
-                    theft, impostor scams, and other forms of financial fraud.</p>
+            <div class="container">
+                <div className="cardHeader">
+                    <h2>Organizations</h2>
+                </div>
+                <div class="resourcesGrid">
+                    <div class="card">
+                        <img src="images/resourcesIcon1.png" alt="Image 1" />
+                        <div className="cardText">
+                            <h3><a href="https://www.ftc.gov/" target="_blank">Federal Trade Commission</a></h3>
+                            <p>The FTC offers information on how to recognize, avoid, and report scams
+                                and fraud. Their website provides tips on protecting yourself from identity
+                                theft, impostor scams, and other forms of financial fraud.</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="images/resourcesIcon2.png" alt="Image 2" />
+                        <div className="cardText">
+                            <h3><a href="https://www.bbb.org/" target="_blank">Better Business Bureau</a></h3>
+                            <p>The BBB provides consumer protection resources, including scam alerts,
+                                tips for avoiding fraud, and information on businesses with questionable practices.</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="images/resourcesIcon3.png" alt="Image 3" />
+                        <div className="cardText">
+                            <h3><a href="https://www.consumerfinance.gov/consumer-tools/fraud/" target="_blank">Consumer Financial Protection Bureau</a></h3>
+                            <p>The CFPB offers resources to help consumers make informed financial decisions
+                                and protect themselves from fraud, including information on banking scams, credit card
+                                fraud, and more.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="images/resourcesIcon4.png" alt="Image 4" />
+                        <div class="cardText">
+                            <h3><a href="https://www.idtheftcenter.org/" target="_blank">Identity Theft Resource Center</a></h3>
+                            <p>The ITRC provides assistance to victims of identity theft and offers resources to
+                                help individuals protect themselves from identity theft and related financial fraud.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="images/resourcesIcon2.png" alt="Image 2"/>
-            <div className="cardText">
-                <h3><a href="https://www.bbb.org/" target="_blank">Better Business Bureau</a></h3>
-                <p>The BBB provides consumer protection resources, including scam alerts, 
-                    tips for avoiding fraud, and information on businesses with questionable practices.</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="images/resourcesIcon3.png" alt="Image 3"/>
-            <div className="cardText">
-                <h3><a href="https://www.consumerfinance.gov/consumer-tools/fraud/" target="_blank">Consumer Financial Protection Bureau</a></h3>
-                <p>The CFPB offers resources to help consumers make informed financial decisions 
-                    and protect themselves from fraud, including information on banking scams, credit card 
-                    fraud, and more.</p>
-            </div>
-        </div>
-        <div className="card">
-            <img src="images/resourcesIcon4.png" alt="Image 4"/>
-            <div class="cardText">
-                <h3><a href="https://www.idtheftcenter.org/" target="_blank">Identity Theft Resource Center</a></h3>
-                <p>The ITRC provides assistance to victims of identity theft and offers resources to 
-                    help individuals protect themselves from identity theft and related financial fraud.</p>
-            </div>
-        </div>
-    </div>
-</div>
 
-<div class="container">
-        <div className="cardHeader"> 
-        <h2>Community Support</h2>
-        </div>
-    <div class="resourcesGrid">
-        <div class="card">
-            <img src="images/resourcesIcon5.png" alt="Image 1"/>
-            <div className="cardText">
-                <h3><a href="https://www.gasa.org/" target="_blank">Global Anti-Scam Alliance</a></h3>
-                <p>sdd blurb</p>
+            <div class="container">
+                <div className="cardHeader">
+                    <h2>Community Support</h2>
+                </div>
+                <div class="resourcesGrid">
+                    <div class="card">
+                        <img src="images/resourcesIcon5.png" alt="Image 1" />
+                        <div className="cardText">
+                            <h3><a href="https://www.gasa.org/" target="_blank">Global Anti-Scam Alliance</a></h3>
+                            <p>The mission of the Global Anti-Scam Alliance is to create a world where people worldwide are safe from
+                                the financial and emotional trauma caused by online scams.</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="images/resourcesIcon6.jpg" alt="Image 2" />
+                        <div className="cardText">
+                            <h3><a href="https://victimconnect.org/" target="_blank">Victim Connect Resource Center</a></h3>
+                            <p>Victim Connect is a referral helpline that connects victims of any crime with resources and support services in their area.
+                                While not specific to financial scams, they can provide assistance and referrals to organizations that offer support for scam victims. </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="images/resourcesIcon7.png" alt="Image 3" />
+                        <div className="cardText">
+                            <h3><a href="https://www.reddit.com/r/Scams/" target="_blank">r/Scams</a></h3>
+                            <p>Reddit is a wide-reaching network community encompassing r/Scams, which is a subreddit focused on educating
+                                about scams.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="images/resourcesIcon8.png" alt="Image 4" />
+                        <div class="cardText">
+                            <h3><a href="https://www.takefive-stopfraud.org.uk/" target="_blank">Take Five To Stop Fraud</a></h3>
+                            <p>Take Five is a national campaign offering straight forward, impartial advice that helps prevent
+                                emails, phone-based and online fraud.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="images/resourcesIcon6.jpg" alt="Image 2"/>
-            <div className="cardText">
-                <h3><a href="https://victimconnect.org/" target="_blank">Victim Connect Resource Center</a></h3>
-                <p>Victim Connect is a referral helpline that connects victims of any crime with resources and support services in their area. 
-                    While not specific to financial scams, they can provide assistance and referrals to organizations that offer support for scam victims. </p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="images/resourcesIcon7.png" alt="Image 3"/>
-            <div className="cardText">
-                <h3><a href="https://www.reddit.com/r/Scams/" target="_blank">r/Scams</a></h3>
-                <p>wrtie soemthin about this reddit community...</p>
-            </div>
-        </div>
-        <div className="card">
-            <img src="images/resourcesIcon8.png" alt="Image 4"/>
-            <div class="cardText">
-                <h3><a href="https://www.takefive-stopfraud.org.uk/" target="_blank">Take Five To Stop Fraud</a></h3>
-                <p>Take Five is a national campaign offering straight forward, impartial advice that helps prevent
-                    emails, phone-based and online fraud.
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
 
 
-<Footer/>
+            <Footer />
         </>
     )
 }
