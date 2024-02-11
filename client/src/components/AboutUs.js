ff7fb13279f0a9ae57548f69152914f58cde31dd
Hannah Kelly <hannahkelly21@gmail.com>
2024-02-10 23:13:08 -0500

    updated content

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -32,9 +32,12 @@ function AboutUs(props) {
         <div className = "textContainer2">
             <div className = "body" >
                <h1>Who are we?</h1>
-               <p>This is filler text.</p>
+               <p>We are a non-profit dedicated to promoting prevention, awareness, and support for the victims of financial scams. Our goal is to spread resources to those affected by this issue, as well as share their stories, to hopefully one day put an end to these scams.</p>
                <h1>What are financial scams?</h1>
-               <p>This is filler text.</p>
+               <p>Financial scams encompass a wide range of fraudulent activities designed to deceive individuals or organizations out of their money or personal financial information. There are many types of financial scams. Some common ones include: <br>
+               </br> <br></br>
+               - <em>Phishing:</em> Scammers impersonate legitimate entities, such as banks or financial institutions, via email, phone calls, or text messages, tricking individuals into providing sensitive information like passwords, credit card numbers, or Social Security numbers. <br></br>
+               </p>
                <h1>How do we solve it?</h1>
                <p>This is filler text.</p>
             </div>
