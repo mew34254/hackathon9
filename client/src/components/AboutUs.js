61c933181aa32862a6839aa643f83fe9bde65642
Hannah Kelly <hannahkelly21@gmail.com>
2024-02-10 21:15:06 -0500

    73fyehiu2

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -11,36 +11,31 @@ function AboutUs(props) {
         <>
         <NavBar />
         <section class = "section1"> 
-            <div className = "AboutUs-image" >
-                <img src = "images/columbiaBlue.png" alt = "About Us Image" />
-            <div className = "AboutUs-text" >
-               <h1>About <br></br> Us</h1>
+        <div className = "initialInterface" >
+            <div className = "textContainer" >
+                <div className = "welcomeText" >
+                    <h1>About <br></br> Us</h1>
+                    <p>This is filler text.</p>
+                </div>
             </div>
-            <div className = "AboutUs-subtext" >
-               <h2>This is filler text.</h2>
-            </div>
-            </div>
+                    <img src = "images/columbiaBlue.png" alt = "About Us Image" />
+        </div>
         </section>
 
         <section class = "body"> 
             <div className = "AboutUs-body" >
                <h1>Who are we?</h1>
-               <h2>This is filler text.</h2>
+               <p>This is filler text.</p>
                <h1>What are financial scams?</h1>
-               <h2>This is filler text.</h2>
+               <p>This is filler text.</p>
                <h1>How do we solve it?</h1>
-               <h2>This is filler text.</h2>
+               <p>This is filler text.</p>
             </div>
-
+                    <div className = "AboutUs-bodyImages" >
+                         <img src = "" alt = "" />
+                    </div>
         </section>
-
-        <section class = "bodyImages"> 
-            <div className = "AboutUs-bodyImages" >
-                <img src = "" alt = "" />
-            </div>
-
-        </section>
-        <Footer />
+        
         </>
     )
 }
